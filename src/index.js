a4775c01c4d798c92488b4bb0663d7c259c55bbd
Anton Dvornikov <anton@MacBook-Pro-Anton.local>
2019-02-20 13:22:11 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+module.exports = function makeExchange(money) {
+  if (money > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+
+  let exchange = {}
+
+  const mapCoinToLetter = {50: 'H', 25: 'Q', 10: 'D', 5: 'N', 1: 'P'}
+  const coins = Object.keys(mapCoinToLetter).reverse()
+
+  while (money > 0) {
+    const coin = coins.shift()
+    let counter = 0
+    while (money-coin > -1) {
+      money -= coin
+      counter++
+    }
+    if (counter) {
+      exchange[mapCoinToLetter[coin]] = counter
+    }
+  }
+
+  return exchange
 }
